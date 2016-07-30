ff6ac7b55f89fe45393660ce15bea6933fcca346
drwecker <d.wecker@techsmith.com>
2016-07-29 22:54:43 -0400

    Added properties for stock price and ticker symbol and bound to UI text boxes.

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -22,6 +23,45 @@ namespace WpfApplication1
     /// </summary>
     public partial class MainWindow : Window
     {
+        //public string strStockName { get; set; }
+        //public string strStockPrice { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _strStockName = "Msft";
+        private string _strStockPrice = "56.21";
+
+        public string strStockName
+        {
+            get { return _strStockName; }
+            set
+            {
+                if( value != _strStockName )
+                {
+                    _strStockName = value;
+                    OnPropertyChanged("strStockName");
+                }
+            }
+        }
+
+        public string strStockPrice
+        {
+            get { return _strStockPrice; }
+            set
+            {
+                if (value != _strStockPrice)
+                {
+                    _strStockPrice = value;
+                    OnPropertyChanged("strStockPrice");
+                }
+            }
+        }
+
+        private void OnPropertyChanged( string v )
+        {
+            throw new NotImplementedException();
+        }
+
         public MainWindow()
         {
             InitializeComponent();
